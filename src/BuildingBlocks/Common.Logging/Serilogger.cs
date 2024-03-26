16dd635ce3a75d9b151539ca614384cc42074d3e
Nam <namnguyenlk5@gmail.com>
2024-03-26 19:39:26 +0700

    Implement logger with serilog

--- src/BuildingBlocks/Common.Logging/Serilogger.cs
+++ src/BuildingBlocks/Common.Logging/Serilogger.cs
@@ -1,7 +1,24 @@
-﻿namespace Common.Logging
+﻿using Microsoft.Extensions.Hosting;
+using Serilog;
+
+namespace Common.Logging
 {
     public class Serilogger
     {
+        public static Action<HostBuilderContext, LoggerConfiguration> Configure => (context, configuration) =>
+        {
+            var applicationName = context.HostingEnvironment.ApplicationName?.ToLower().Replace(".","-");
+            var enviromentName = context.HostingEnvironment.EnvironmentName ?? "Development";
 
+            configuration
+                .WriteTo.Debug()
+                .WriteTo.Console(outputTemplate:
+                "[{Timestamp:HH:mm:ss}{Level}]{SourceContext}{NewLine}{Message:lj}{NewLine}{Exeption}{NewLine}")
+                .Enrich.FromLogContext()
+                .Enrich.WithMachineName()
+                .Enrich.WithProperty("Enviroment", enviromentName)
+                .Enrich.WithProperty("Application", applicationName)
+                .ReadFrom.Configuration(context.Configuration);
+        };
     }
 }
